41aecf1909c482c82c77b6c58be81a8613c5d477
thrall6000 <thrall6000@gmail.com>
2023-03-24 18:36:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,78 +1,94 @@
 const gameBoard = document.getElementById('gameBoard');
-const images = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+const resetButton = document.getElementById('resetButton');
+const images = [
+  'image1.png', 'image2.png', 'image3.png', 'image4.png',
+  'image5.png', 'image6.png', 'image7.png', 'image8.png',
+  'image1.png', 'image2.png', 'image3.png', 'image4.png',
+  'image5.png', 'image6.png', 'image7.png', 'image8.png'
+];
 let firstTile, secondTile;
 let isProcessing = false;
 
+shuffle(images);
+createTiles();
+
+resetButton.addEventListener('click', resetGame);
+
 function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
 function createTiles() {
-    const shuffledImages = shuffle(images);
-    shuffledImages.forEach(image => {
-        const tile = document.createElement('div');
-        tile.classList.add('tile');
-        tile.textContent = image;
-        tile.addEventListener('click', handleTileClick);
-        gameBoard.appendChild(tile);
-    });
+  const shuffledImages = shuffle(images);
+  shuffledImages.forEach((image, index) => {
+    const tile = document.createElement('div');
+    tile.classList.add('tile');
+    tile.style.backgroundImage = `url('${image}')`;
+    tile.dataset.image = image;
+    tile.addEventListener('click', handleTileClick);
+    gameBoard.appendChild(tile);
+  });
 }
 
 function handleTileClick(event) {
-    if (isProcessing) return;
+  if (isProcessing) return;
 
-    const tile = event.target;
+  const tile = event.target;
 
-    if (tile.classList.contains('revealed') || firstTile === tile) {
-        return;
-    }
+  if (tile.classList.contains('revealed') || firstTile === tile) {
+    return;
+  }
 
-    tile.classList.add('revealed');
-    tile.style.color = 'white';
+  tile.classList.add('revealed');
 
-    if (!firstTile) {
-        firstTile = tile;
+  if (!firstTile) {
+    firstTile = tile;
+  } else {
+    secondTile = tile;
+
+    if (firstTile.dataset.image === secondTile.dataset.image) {
+      firstTile = null;
+      secondTile = null;
     } else {
-        secondTile = tile;
-
-        if (firstTile.textContent === secondTile.textContent) {
-            firstTile = null;
-            secondTile = null;
-        } else {
-            isProcessing = true;
-            disableBoard();
-            setTimeout(() => {
-                firstTile.classList.remove('revealed');
-                firstTile.style.color = 'transparent';
-                secondTile.classList.remove('revealed');
-                secondTile.style.color = 'transparent';
-                firstTile = null;
-                secondTile = null;
-                enableBoard();
-                isProcessing = false;
-            }, 1000);
-        }
+      isProcessing = true;
+      disableBoard();
+      setTimeout(() => {
+        firstTile.classList.remove('revealed');
+        secondTile.classList.remove('revealed');
+        firstTile = null;
+        secondTile = null;
+        enableBoard();
+        isProcessing = false;
+      }, 1000);
     }
+  }
 }
 
 function disableBoard() {
-    const tiles = document.querySelectorAll('.tile');
-    tiles.forEach(tile => {
-        tile.style.pointerEvents = 'none';
-    });
+  const tiles = document.querySelectorAll('.tile');
+  tiles.forEach(tile => {
+    tile.style.pointerEvents = 'none';
+  });
 }
 
 function enableBoard() {
-    const tiles = document.querySelectorAll('.tile');
-    tiles.forEach(tile => {
-        if (!tile.classList.contains('revealed')) {
-            tile.style.pointerEvents = 'auto';
-        }
-    });
+  const tiles = document.querySelectorAll('.tile');
+  tiles.forEach(tile => {
+    if (!tile.classList.contains('revealed')) {
+      tile.style.pointerEvents = 'auto';
+    }
+  });
 }
 
-createTiles();
+function resetGame() {
+  gameBoard.innerHTML = '';
+  shuffle(images);
+  createTiles();
+  firstTile = null;
+  secondTile = null;
+  isProcessing = false;
+}
